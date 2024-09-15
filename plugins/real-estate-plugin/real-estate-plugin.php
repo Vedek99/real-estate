cc11d5a285e38308fe806e41fbfcc2f07b10288a
Vedek99merin <vadim@codix.co>
2024-09-15 18:16:26 +0300

    added plugin and some fixes for widget

--- plugins/real-estate-plugin/real-estate-plugin.php
+++ plugins/real-estate-plugin/real-estate-plugin.php
@@ -116,3 +116,146 @@ function real_estate_duplicate_post() {
     }
 }
 add_action('admin_action_duplicate_real_estate', 'real_estate_duplicate_post');
+
+add_shortcode('real_estate_filter', 'real_estate_filter_shortcode');
+
+function filter_real_estate() {
+    $paged = isset($_GET['page']) ? intval($_GET['page']) : 1;
+    $posts_per_page = 5;
+
+    $args = array(
+        'post_type' => 'real_estate',
+        'posts_per_page' => $posts_per_page,
+        'paged' => $paged,
+        'meta_query' => array()
+    );
+
+    if (isset($_GET['building_name']) && !empty($_GET['building_name'])) {
+        $args['meta_query'][] = array(
+            'key' => 'building_name',
+            'value' => sanitize_text_field($_GET['building_name']),
+            'compare' => 'LIKE'
+        );
+    }
+
+    if (isset($_GET['location_coordinates']) && !empty($_GET['location_coordinates'])) {
+        $args['meta_query'][] = array(
+            'key' => 'location_coordinates',
+            'value' => sanitize_text_field($_GET['location_coordinates']),
+            'compare' => 'LIKE'
+        );
+    }
+
+    if (isset($_GET['number_of_floors']) && !empty($_GET['number_of_floors'])) {
+        $args['meta_query'][] = array(
+            'key' => 'number_of_floors',
+            'value' => intval($_GET['number_of_floors']),
+            'compare' => '='
+        );
+    }
+
+    if (isset($_GET['type_of_building']) && !empty($_GET['type_of_building'])) {
+        $args['meta_query'][] = array(
+            'key' => 'type_of_building',
+            'value' => sanitize_text_field($_GET['type_of_building']),
+            'compare' => '='
+        );
+    }
+
+    if (isset($_GET['environmental_friendliness']) && !empty($_GET['environmental_friendliness'])) {
+        $args['meta_query'][] = array(
+            'key' => 'environmental_friendliness',
+            'value' => intval($_GET['environmental_friendliness']),
+            'compare' => '='
+        );
+    }
+
+    $query = new WP_Query($args);
+
+    ob_start(); 
+
+    if ($query->have_posts()) :
+        while ($query->have_posts()) : $query->the_post(); ?>
+            <a class="real-estate-details" href="<?php the_permalink(); ?>">
+                <div class="details-image">
+                    <div class="about-building">
+                        <h2><?php the_title(); ?></h2>
+                        <p><strong><?php esc_html_e('Назва будинку:', 'text-domain'); ?></strong> <?php the_field('building_name'); ?></p>
+                        <p><strong><?php esc_html_e('Координати місцезнаходження:', 'text-domain'); ?></strong> <?php the_field('location_coordinates'); ?></p>
+                        <p><strong><?php esc_html_e('Кількість поверхів:', 'text-domain'); ?></strong> <?php the_field('number_of_floors'); ?></p>
+                        <p><strong><?php esc_html_e('Тип будівлі:', 'text-domain'); ?></strong> <?php the_field('type_of_building'); ?></p>
+                        <p><strong><?php esc_html_e('Екологічність:', 'text-domain'); ?></strong> <?php the_field('environmental_friendliness'); ?></p>
+                    </div>
+                    <?php if (get_field('image')) : ?>
+                        <img src="<?php the_field('image'); ?>" alt="Зображення будівлі">
+                    <?php endif; ?>
+                </div>
+            </a>
+        <?php endwhile;
+
+        $pagination_args = array(
+            'total' => $query->max_num_pages,
+            'current' => $paged,
+            'format' => '?page=%#%',
+            'prev_text' => __('« Previous', 'text-domain'),
+            'next_text' => __('Next »', 'text-domain'),
+            'before_page_number' => '<a href="#" data-page="%#%">',
+            'after_page_number' => '</a>'
+        );
+        $pagination = paginate_links($pagination_args);
+        echo '<div class="pagination">' . $pagination . '</div>';
+
+    else :
+        echo '<p>Немає об\'єктів для відображення.</p>';
+    endif;
+
+    wp_reset_postdata();
+
+    wp_send_json(array(
+        'html' => ob_get_clean(), 
+        'pagination' => $pagination 
+    ));
+}
+add_action('wp_ajax_filter_real_estate', 'filter_real_estate');
+add_action('wp_ajax_nopriv_filter_real_estate', 'filter_real_estate');
+
+class RealEstateFilterWidget extends WP_Widget {
+    
+    public function __construct() {
+        parent::__construct(
+            'real_estate_filter_widget',
+            __('Фільтр нерухомості вiджет', 'test-domain'),
+            array('description' => __('Відображає фільтр нерухомості.', 'test-domain'))
+        );
+    }
+
+    public function widget($args, $instance) {
+        echo $args['before_widget'];
+        echo $args['before_title'] . apply_filters('widget_title', $instance['title']) . $args['after_title'];
+
+        echo do_shortcode('[real_estate_filter]');
+
+        echo $args['after_widget'];
+    }
+
+    public function form($instance) {
+        $title = !empty($instance['title']) ? $instance['title'] : __('Фільтр нерухомості вiджет', 'test-domain');
+        ?>
+        <p>
+            <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Заголовок:'); ?></label> 
+            <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>">
+        </p>
+        <?php 
+    }
+
+    public function update($new_instance, $old_instance) {
+        $instance = array();
+        $instance['title'] = (!empty($new_instance['title'])) ? strip_tags($new_instance['title']) : '';
+        return $instance;
+    }
+}
+
+function register_real_estate_filter_widget() {
+    register_widget('RealEstateFilterWidget');
+}
+add_action('widgets_init', 'register_real_estate_filter_widget');
--- themes/understrap-child/templates/home.php
+++ themes/understrap-child/templates/home.php
@@ -5,7 +5,6 @@ get_header();
 ?>
 
 <main>
-    <?php echo do_shortcode('[real_estate_filter]') ?>
     <?php if ( is_active_sidebar( 'left-sidebar' ) ) : ?>
     <aside id="secondary" class="widget-area">
         <?php dynamic_sidebar( 'left-sidebar' ); ?>
